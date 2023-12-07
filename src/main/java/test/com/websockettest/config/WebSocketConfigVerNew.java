6b04bf98c58e88300b7f1bc7fca8f0ea0b976f45
daotrongthuan1010 <conxinloime1010@gmail.com>
2023-12-07 22:00:36 +0700

    [WebSocket]: Test websocket chat message real time

---
+++ src/main/java/test/com/websockettest/config/WebSocketConfigVerNew.java
@@ -0,0 +1,14 @@
+package test.com.websockettest.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfigVerNew implements WebSocketMessageBrokerConfigurer {
+
+
+
+
+}
--- src/main/java/test/com/websockettest/model/ChatMessage.java
+++ src/main/java/test/com/websockettest/model/ChatMessage.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 @Data
 public class ChatMessage {
+
     private String content;
 
     private String sender;
